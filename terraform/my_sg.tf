cd7e97fc599fe29b5de2bf7690f62963039da55c
daniel-bacl <95eksldpf@gmail.com>
2025-03-31 06:11:11 +0000

    add terraform file

---
+++ terraform/my_sg.tf
@@ -0,0 +1,18 @@
+resource "aws_security_group" "my_sg" {
+	vpc_id = aws_vpc.my_vpc.id
+	egress {
+		cidr_blocks = ["0.0.0.0/0"]
+		from_port = 0
+		to_port = 0
+		protocol = "-1"
+	}
+	ingress {
+		cidr_blocks = ["0.0.0.0/0"]
+		from_port = 22
+		to_port = 22
+		protocol = "tcp"
+	}
+	tags = {
+		Name = "my-sg"
+	}
+}
